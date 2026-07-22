37922cbc38b5b3c1841ad6662c6939129ae26e4f
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-22 22:10:18 +0300

    three nums summ app

--- summ.cpp
+++ summ.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main() {
 
-  int first_number, second_number, sum;
+  int first_number, second_number, third_number, sum;
     
   cout << "Enter two nums: ";
-  cin >> first_number >> second_number;
+  cin >> first_number >> second_number >> third_number;
 
   // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number;
+  sum = first_number + second_number + third_number;
 
   // prints sum s
-  cout << first_number << " + " <<  second_number << " = " << sum;     
+  cout << first_number << " + " <<  second_number << " + " <<  third_number << " = " << sum;     
 
   return 0;
 }
