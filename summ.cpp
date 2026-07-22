eff12333042faf61dfb008cfdfb72e59f1fb439c
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-22 22:02:03 +0300

    first stage summ app

---
+++ summ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+    
+  cout << "Enter two nums: ";
+  cin >> first_number >> second_number;
+
+
+  return 0;
+}
