6307b34dad3767555dbdcedeb5aba2f4de1ec5c9
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-23 23:08:05 +0300

    new changes sum app

--- summ.cpp
+++ summ.cpp
@@ -5,7 +5,7 @@ int main() {
 
   int first_number, second_number, third_number, sum;
     
-  cout << "Enter two nums: ";
+  cout << "Enter three nums: ";
   cin >> first_number >> second_number >> third_number;
 
   // sum of two numbers in stored in variable sumOfTwoNumbers
