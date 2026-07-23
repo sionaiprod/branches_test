7f5a9c75993d44625eb5187efdf34db056e6ca60
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-23 23:22:01 +0300

    with func

--- summ.cpp
+++ summ.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int addNumbers(int num1, int num2) {
+    int sum = num1 + num2;
+    return sum; // Returns the result to the caller
+}
 int main() {
 
-  int first_number, second_number, third_number, sum;
+  int first_number, second_number, sum;
     
-  cout << "Enter three nums: ";
-  cin >> first_number >> second_number >> third_number;
+  cout << "Enter two nums: ";
+  cin >> first_number >> second_number;
 
   // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number + third_number;
+  sum = addNumbers(first_number, second_number);
 
   // prints sum s
-  cout << first_number << " + " <<  second_number << " + " <<  third_number << " = " << sum;     
+  cout << first_number << " + " <<  second_number << " = " << sum;     
 
   return 0;
 }
